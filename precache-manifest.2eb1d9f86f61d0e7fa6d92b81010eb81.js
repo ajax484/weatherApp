94668da86ce3dc09a7344d94b27c69fd08550a27
Yusuf Bilal <ubahyusuf484@gmail.com>
2022-03-30 15:08:53 +0100

    Updates

---
+++ precache-manifest.2eb1d9f86f61d0e7fa6d92b81010eb81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e686dc221f46609df2481e2d3ccc3c9e",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "9fd7a0e994b366c28ab3",
+    "url": "/weatherApp/static/css/main.61f15c51.chunk.css"
+  },
+  {
+    "revision": "5f2d7f5d769d29b62c98",
+    "url": "/weatherApp/static/js/2.1a328b74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.1a328b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd7a0e994b366c28ab3",
+    "url": "/weatherApp/static/js/main.c6a2284c.chunk.js"
+  },
+  {
+    "revision": "07494438169704d3990a",
+    "url": "/weatherApp/static/js/runtime-main.993d4fbc.js"
+  },
+  {
+    "revision": "7d89e18be25529a13d86b4591aa832d6",
+    "url": "/weatherApp/static/media/pexels-lumn-167699.7d89e18b.jpg"
+  }
+]);
